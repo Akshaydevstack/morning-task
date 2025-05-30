9e89c42305451d8f9fc3bc6cf6bd7b09e7358604
MRflutter0621 <mrflutter421@gmail.com>
2025-05-30 15:48:10 +0530

    task two

---
+++ largest.js
@@ -0,0 +1,11 @@
+function largest(num){
+    let big=num[0];
+    for(let i=0;i<num.length;i++){
+    if(big<num[i]){
+        big=num[i]
+    }
+    }
+    return big;
+}
+
+console.log(largest([2,5,7,8,0,3]));
